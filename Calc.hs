2bfce764868efa2ec0669dae7b86bdc21a285a64
Andrew Levenson <andrew.levenson@gmail.com>
2011-03-27 22:06:23 -0400

    Now parses expressions prior to evaluating them!

--- Calc.hs
+++ Calc.hs
@@ -1,6 +1,9 @@
+import RDParser
 import ShuntingYard
 import PostfixEval
 import Tokenizer
 
 calc :: String -> Int
-calc = postfixEval . shuntingYard . tokenize
+calc exp = if parse exp  
+           then postfixEval . shuntingYard . tokenize $ exp
+           else error "Invalid expression."
